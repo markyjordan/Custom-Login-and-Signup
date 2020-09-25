c5d91c0245eb089066de9305060900a839d02a63
Mark Jordan <markybjordan@gmail.com>
2020-09-24 18:21:21 -0700

    Created signup, login and home VCs.

---
+++ Custom Login and Signup/Controller/SignupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignupViewController.swift
+//  Custom Login and Signup
+//
+//  Created by Marky Jordan on 9/24/20.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
