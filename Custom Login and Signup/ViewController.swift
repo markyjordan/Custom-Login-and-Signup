0903f2bca2d344855b0969d7c7d02478cd7703b3
Mark Jordan <markybjordan@gmail.com>
2020-09-24 17:58:42 -0700

    Created a new project.
    Added all the default files and folders to the repository.

---
+++ Custom Login and Signup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Login and Signup
+//
+//  Created by Marky Jordan on 9/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
