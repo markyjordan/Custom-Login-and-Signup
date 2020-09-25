0eae6abcf254156f41581a548f8079878ec1130e
Mark Jordan <markybjordan@gmail.com>
2020-09-25 11:34:02 -0700

    Added IBOutlets and IBActions for signup VC.

--- Custom Login and Signup/Controller/SignupViewController.swift
+++ Custom Login and Signup/Controller/SignupViewController.swift
@@ -9,21 +9,26 @@ import UIKit
 
 class SignupViewController: UIViewController {
 
+    // MARK: - Outlets/Properties
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+  
+    // MARK: - Actions
+    
+    @IBAction func signupButtonTapped(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
