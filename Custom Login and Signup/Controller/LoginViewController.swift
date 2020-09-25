f0676dc3fb5e9852f257d2570ebe2572005550e3
Mark Jordan <markybjordan@gmail.com>
2020-09-25 11:43:21 -0700

    Added IBOutlets and IBActions to login VC.

--- Custom Login and Signup/Controller/LoginViewController.swift
+++ Custom Login and Signup/Controller/LoginViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: - Outlets/Properties
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
     }
-    */
-
+    
 }
