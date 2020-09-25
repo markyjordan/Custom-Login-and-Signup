c4f114413cdfa9b3980cc7225713739504942b26
Mark Jordan <markybjordan@gmail.com>
2020-09-25 16:25:00 -0700

    Created a new class for ui element styling.
    Included a method to create a bottom line on a text field.

---
+++ Custom Login and Signup/Helpers/Styles.swift
@@ -0,0 +1,28 @@
+//
+//  Styles.swift
+//  Custom Login and Signup
+//
+//  Created by Marky Jordan on 9/25/20.
+//
+
+import Foundation
+import UIKit
+
+class Styles {
+    
+    static func styleTextField(_ textField: UITextField) {
+        
+        // construct the bottom line layer
+        let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textField.frame.height - 2, width: textField.frame.width, height: 2)
+        bottomLine.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1).cgColor
+        
+        // remove text field border
+        textField.borderStyle = .none
+        
+        // add the bottom line to the text field
+        textField.layer.addSublayer(bottomLine)
+    }
+    
+}
